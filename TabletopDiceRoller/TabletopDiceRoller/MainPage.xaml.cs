e7471f5569aa47ca7c4865354281494aa237d783
Kevin <klgvette07@gmail.com>
2019-01-30 04:16:51 -0500

    implemented start of custom roll interface

--- TabletopDiceRoller/TabletopDiceRoller/MainPage.xaml.cs
+++ TabletopDiceRoller/TabletopDiceRoller/MainPage.xaml.cs
@@ -7,8 +7,10 @@ using Xamarin.Forms;
 
 namespace TabletopDiceRoller
 {
-    public partial class MainPage : ContentPage
+    public partial class MainPage : TabbedPage
     {
+        private Random rnd = new Random(Environment.TickCount);
+
         public MainPage()
         {
             InitializeComponent();
@@ -23,8 +25,7 @@ namespace TabletopDiceRoller
         }        
 
         private int Roll(string value)
-        {
-            Random rnd = new Random(Environment.TickCount);
+        {            
             int rolled = rnd.Next(1, Convert.ToInt32(value));
             return rolled;
         }
@@ -33,6 +34,70 @@ namespace TabletopDiceRoller
         {
             var popUpPage = new DiceRoll(die, roll, values);
             await Navigation.PushModalAsync(popUpPage, false);      
+        }
+
+        private void OnCustomClick(object sender, EventArgs e)
+        {
+            string input = CustomRollInput.Text;
+            string[] roll = input.Split('+', '-');
+            string outputRoll = "";
+            foreach (string r in roll)
+            {
+                if (r.Contains('d'))
+                {
+                    string[] diceRoll = new string[2];
+                    
+                    try
+                    {                        
+                        diceRoll = r.Split('d');
+                        if (diceRoll.Length != 2)
+                        {
+                            throw new Exception();
+                        }
+                        string[] diceResult = new string[Convert.ToInt32(diceRoll[0])];
+                        string diceValue = (Convert.ToInt32(diceRoll[1]) + 1).ToString();
+                        for (int i = 0; i < Convert.ToInt32(diceRoll[0]) ; i++)
+                        {                            
+                            diceResult[i] = Roll(diceValue).ToString();
+                        }
+                        string rollValue = "";
+                        foreach (string result in diceResult)
+                        {
+                            if (rollValue == "")
+                            {
+                                rollValue += result;
+                            }
+                            else
+                            {
+                                rollValue += "+" + result;
+                            }
+                        }
+                        outputRoll += rollValue + " ";
+                    }
+                    catch (Exception ex)
+                    {
+                        outputRoll = "Improper format. e.g. 8d6+1d4+1";
+                        break;
+                    }                    
+                }
+                else
+                {
+                    outputRoll += r + " ";
+                }                
+            }
+            CustomOutput.Text = outputRoll;
+        }
+
+        private void OnCustomInput(Button button, EventArgs e)
+        {
+            if (button.Text == "Del")
+            {
+                CustomRollInput.Text = "";
+            }
+            else
+            {
+                CustomRollInput.Text += button.Text;
+            }            
         }
     }
 }
